fb4c3df8d4f82c06fb1a15a4a77835e3bcc4b808
Aigamo <ycanardeau@gmail.com>
2023-07-11 12:37:50 +0000

    Replace hostname

--- frontend/src/components/BookmarkCreateModal.tsx
+++ frontend/src/components/BookmarkCreateModal.tsx
@@ -18,6 +18,33 @@ import {
 import { OpenRegular } from '@fluentui/react-icons';
 import React from 'react';
 
+const normalizedHosts: Record<string, string> = {
+	// https://en.wikipedia.org/wiki/Amazon_(company)#Products_and_services
+	'amazon.ae': 'amazon.com',
+	'amazon.ca': 'amazon.com',
+	'amazon.cn': 'amazon.com',
+	'amazon.co.jp': 'amazon.com',
+	'amazon.co.uk': 'amazon.com',
+	'amazon.com.au': 'amazon.com',
+	'amazon.com.be': 'amazon.com',
+	'amazon.com.br': 'amazon.com',
+	'amazon.com.mx': 'amazon.com',
+	'amazon.com.tr': 'amazon.com',
+	'amazon.de': 'amazon.com',
+	'amazon.eg': 'amazon.com',
+	'amazon.es': 'amazon.com',
+	'amazon.fr': 'amazon.com',
+	'amazon.in': 'amazon.com',
+	'amazon.it': 'amazon.com',
+	'amazon.nl': 'amazon.com',
+	'amazon.pl': 'amazon.com',
+	'amazon.sa': 'amazon.com',
+	'amazon.se': 'amazon.com',
+	'amazon.sg': 'amazon.com',
+
+	'music.youtube.com': 'youtube.com',
+};
+
 function normalizeUrl(url: URL): URL {
 	const newUrl = new URL(url.href);
 
@@ -28,6 +55,17 @@ function normalizeUrl(url: URL): URL {
 	const parts = url.hostname.split('.');
 	if (parts[0] === 'www') {
 		newUrl.hostname = parts.slice(1).join('.');
+	}
+
+	return newUrl;
+}
+
+function replaceHostname(url: URL): URL {
+	const newUrl = new URL(url.href);
+
+	const normalizedHost = normalizedHosts[url.hostname];
+	if (normalizedHost !== undefined) {
+		newUrl.hostname = normalizedHost;
 	}
 
 	return newUrl;
@@ -90,7 +128,12 @@ export const BookmarkCreateModal = ({
 					onClick={async (): Promise<void> => {
 						setIsLoading(true);
 
-						const urlFetchResult = await urlApi.fetch({ url: url });
+						const replacedUrl = replaceHostname(
+							normalizeUrl(new URL(url)),
+						).href;
+						const urlFetchResult = await urlApi.fetch({
+							url: replacedUrl,
+						});
 						if (!urlFetchResult.ok) {
 							setIsLoading(false);
 							return;
@@ -98,7 +141,7 @@ export const BookmarkCreateModal = ({
 
 						const { title, canonical } = urlFetchResult.val;
 						const normalizedUrl = normalizeUrl(
-							new URL(canonical || url),
+							new URL(canonical || replacedUrl),
 						).href;
 
 						const noteCreateResult = await noteApi.create({
