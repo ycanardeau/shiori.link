5bfa2a6637c605bad715ed26525f9023f59af200
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2024-06-05 21:57:11 +0900

    Update entities and entitiesTs in mikro-orm.config.ts

--- backend/app/server/src/mikro-orm.config.ts
+++ backend/app/server/src/mikro-orm.config.ts
@@ -1,11 +1,9 @@
 import { Migrator } from '@mikro-orm/migrations';
 import { defineConfig } from '@mikro-orm/mysql';
-import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 
 export default defineConfig({
 	highlighter: new SqlHighlighter(),
-	metadataProvider: TsMorphMetadataProvider,
 	migrations: {
 		snapshotName: '.snapshot',
 		path: './dist/migrations',
@@ -15,8 +13,14 @@ export default defineConfig({
 	schemaGenerator: {
 		disableForeignKeys: false,
 	},
-	entities: ['./dist/entities/**/*.js'],
-	entitiesTs: ['./src/entities/**/*.ts'],
+	entities: [
+		// FIXME: Replace domain with infrastructure.
+		'./node_modules/@shiori.link/server.monolith.domain/dist/entities',
+	],
+	entitiesTs: [
+		// FIXME: Replace domain with infrastructure.
+		'./node_modules/@shiori.link/server.monolith.domain/src/entities',
+	],
 	forceUndefined: true,
 	forceUtcTimezone: true,
 	allowGlobalContext: false,
